e1bd4fc2a91708ea7e7ed12665b869a60c987297
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-05-21 11:26:19 +0200

    test Vercel deployment

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,10 +1,10 @@
-export const revalidate = 420; // ISR revalidation period in seconds
+// export const revalidate = 420; // ISR revalidation period in seconds
 
-interface Post {
-  title: string;
-  content: string;
-  slug: string;
-}
+// interface Post {
+//   title: string;
+//   content: string;
+//   slug: string;
+// }
 
 interface Props {
   params: {
@@ -12,35 +12,55 @@ interface Props {
   };
 }
 
-// Dynamic data that doesn't change often (e.g. blog posts)
-export async function generateStaticParams() {
-  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
-    (res) => res.json()
-  );
-
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
-}
-
 export default async function BlogPostPage({ params }: Props) {
-  const posts: Post[] = await fetch("http://localhost:3000/api/content").then(
-    (res) => res.json()
-  );
-  const post = posts.find((post) => post.slug === params.slug);
-
-  if (!post) {
-    return (
-      <div>
-        <h1>Post not found</h1>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.content}</p>
-    </div>
-  );
+  return <h1>Post not found</h1>;
 }
+
+// import { CONTENT_API_URL } from "@/config";
+
+// export const revalidate = 420; // ISR revalidation period in seconds
+
+// interface Post {
+//   title: string;
+//   content: string;
+//   slug: string;
+// }
+
+// interface Props {
+//   params: {
+//     slug: string;
+//   };
+// }
+
+// // Dynamic data that doesn't change often (e.g. blog posts)
+// export async function generateStaticParams() {
+//   const posts: Post[] = await fetch(`${CONTENT_API_URL}/api/content`).then(
+//     (res) => res.json()
+//   );
+
+//   return posts.map((post) => ({
+//     slug: post.slug,
+//   }));
+// }
+
+// export default async function BlogPostPage({ params }: Props) {
+//   const posts: Post[] = await fetch(`${CONTENT_API_URL}/api/content`).then(
+//     (res) => res.json()
+//   );
+//   const post = posts.find((post) => post.slug === params.slug);
+
+//   if (!post) {
+//     return (
+//       <div>
+//         <h1>Post not found</h1>
+//       </div>
+//     );
+//   }
+
+//   return (
+//     <div>
+//       <h1>{post.title}</h1>
+//       <p>{post.content}</p>
+//     </div>
+//   );
+// }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,31 @@
-import Link from "next/link";
-
 export default async function Blog() {
-  const posts = await fetch("http://localhost:3000/api/content").then((res) =>
-    res.json()
-  );
-
   return (
     <div>
       <h1 className="mb-8">Welcome to our Blog</h1>
-
-      <ul>
-        {posts.map((post: any) => (
-          <li key={post.slug} className="my-2">
-            <Link href={`/blog/${post.slug}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
+
+// import Link from "next/link";
+
+// import { CONTENT_API_URL } from "@/config";
+
+// export default async function Blog() {
+//   const posts = await fetch(`${CONTENT_API_URL}/api/content`).then((res) =>
+//     res.json()
+//   );
+
+//   return (
+//     <div>
+//       <h1 className="mb-8">Welcome to our Blog</h1>
+
+//       <ul>
+//         {posts.map((post: any) => (
+//           <li key={post.slug} className="my-2">
+//             <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
